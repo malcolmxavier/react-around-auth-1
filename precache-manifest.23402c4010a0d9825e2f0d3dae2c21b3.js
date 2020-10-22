a9a049af84b34acc57914cd85c86fb43cccb8be8
Jenna Toff <shamanlvr@gmail.com>
2020-10-22 15:51:16 -0700

    Updates

---
+++ precache-manifest.23402c4010a0d9825e2f0d3dae2c21b3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1add5c76c3f02fd41e58e639e1763eef",
+    "url": "/react-around-auth/index.html"
+  },
+  {
+    "revision": "bb090ea22bd3df8c5dd0",
+    "url": "/react-around-auth/static/css/main.10db1b45.chunk.css"
+  },
+  {
+    "revision": "d52e0bc64b1a742ac931",
+    "url": "/react-around-auth/static/js/2.7961479a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-around-auth/static/js/2.7961479a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb090ea22bd3df8c5dd0",
+    "url": "/react-around-auth/static/js/main.8b5b2322.chunk.js"
+  },
+  {
+    "revision": "5f473114918419229a6d",
+    "url": "/react-around-auth/static/js/runtime-main.75500119.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/react-around-auth/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/react-around-auth/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/react-around-auth/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "/react-around-auth/static/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/react-around-auth/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/react-around-auth/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/react-around-auth/static/media/add-icon.b2fce4ac.svg"
+  },
+  {
+    "revision": "8f7611ae39c682f37516feec261f77dd",
+    "url": "/react-around-auth/static/media/around_the_us.8f7611ae.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/react-around-auth/static/media/close-icon.5df32a06.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/react-around-auth/static/media/edit-pencil.6bfdb354.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/react-around-auth/static/media/heart-filled.5bbb05f7.svg"
+  },
+  {
+    "revision": "83a46f45b0ce22c9a4433c11d0372f26",
+    "url": "/react-around-auth/static/media/heart.83a46f45.svg"
+  },
+  {
+    "revision": "f202ad3053ae4410e6d246994df87581",
+    "url": "/react-around-auth/static/media/trash.f202ad30.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8b8d0ed501859aade4ee110cf66a770.js"
+  "/react-around-auth/precache-manifest.23402c4010a0d9825e2f0d3dae2c21b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-around-auth/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
